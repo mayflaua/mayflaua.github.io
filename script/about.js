f353ca23f92c9187798a73f1df61d82d69279b3f
sweetie <num1carbon@gmail.com>
2022-05-21 14:08:27 +0300

    snake game added to the main page, about page categories  updated

--- script/about.js
+++ script/about.js
@@ -1,3 +1,24 @@
+function toList(string) {
+  string = string.split(' ');
+  var lines = [];
+
+  for (var i=0; i<string.length; i+=7) {
+    lines.push(string.slice(i, i+7).join(' '));
+  }
+  
+  var result = '';
+  for (var i=0; i<lines.length; i++) {
+    result += li(lines[i]);
+  }
+  
+  return result
+}
+
+function li(string) {
+  return "<li class=\"content-window-text__stroke\">" + string + "</li>";
+
+}
+
 let profTab = document.querySelector(".aside-prof-info");
 let persTab = document.querySelector(".aside-pers-info");
 let hobbiesTab = document.querySelector(".aside-hobbies");
@@ -25,17 +46,69 @@ hobbiesTab.addEventListener("click", function() {
 
 let navTitle = document.querySelector(".nav-tree__title");
 
+
+let workFolder = document.querySelector(".nav-tree__work");
 // aside buttons
 function switchProffesional() {
-  navTitle.textContent = "professional-info";
+  // navTitle.innerHTML = "<div class=\"cat-arrow cat-arrow__pers\"></div>professional-info";
+  workFolder.classList.remove("invisible");
+  bioFolder.classList.add("invisible");
+  interestsFolder.classList.add("invisible");
+  educationFolder.classList.add("invisible");
+  document.querySelector(".nav-tree__bio_childhood").classList.add("invisible");
+  document.querySelector(".nav-tree__bio_present").classList.add("invisible");
+  document.querySelector(".nav-tree__interests_code").classList.add("invisible");
+  document.querySelector(".nav-tree__interests_tech").classList.add("invisible");
+  document.querySelector(".nav-tree__education_school").classList.add("invisible");
+  document.querySelector(".nav-tree__education_university").classList.add("invisible");
+  interestsFolder.firstChild.classList.remove("folder-arrow_opened");
+  educationFolder.firstChild.classList.remove("folder-arrow_opened");
+  bioFolder.firstChild.classList.remove("folder-arrow_opened");
+  if (catArrowPersonal.classList.contains("cat-arrow_opened")) {
+    navTitle.innerHTML = "<div class=\"cat-arrow cat-arrow__pers cat-arrow_opened\"></div>professional-info";
+  }
+  else {
+    navTitle.innerHTML = "<div class=\"cat-arrow cat-arrow__pers\"></div>professional-info";
+  }
+  catArrowPersonal = document.querySelector(".cat-arrow__pers");
 }
 
 function switchPersonal() {
-  navTitle.textContent = "personal-info";
+  // navTitle.innerHTML = "<div class=\"cat-arrow cat-arrow__pers\"></div>personal-info";
+  workFolder.classList.add("invisible");
+  bioFolder.classList.remove("invisible");
+  interestsFolder.classList.remove("invisible");
+  educationFolder.classList.remove("invisible");
+  workFolder.firstChild.classList.remove("folder-arrow_opened");
+  catArrowPersonal = document.querySelector(".cat-arrow__pers");
+  document.querySelector(".nav-tree__work_catering").classList.add("invisible");
+  if (catArrowPersonal.classList.contains("cat-arrow_opened")) {
+    navTitle.innerHTML = "<div class=\"cat-arrow cat-arrow__pers cat-arrow_opened\"></div>personal-info";
+  }
+  else {
+    navTitle.innerHTML = "<div class=\"cat-arrow cat-arrow__pers\"></div>personal-info";
+  }
+  catArrowPersonal = document.querySelector(".cat-arrow__pers");
 }
 
 function switchHobbies() {
-  navTitle.textContent = "hobbies";
+  // navTitle.innerHTML = "<div class=\"cat-arrow cat-arrow__pers\"></div>hobbies";
+  catArrowPersonal = document.querySelector(".cat-arrow__pers");
+  document.querySelector(".nav-tree__work_catering").classList.add("invisible");
+  workFolder.classList.add("invisible");
+  bioFolder.classList.add("invisible");
+  interestsFolder.classList.remove("invisible");
+  educationFolder.classList.add("invisible");
+  workFolder.firstChild.classList.remove("folder-arrow_opened");
+
+  document.querySelector(".nav-tree__work_catering").classList.add("invisible");
+  if (catArrowPersonal.classList.contains("cat-arrow_opened")) {
+    navTitle.innerHTML = "<div class=\"cat-arrow cat-arrow__pers cat-arrow_opened\"></div>hobbies ";
+  }
+  else {
+    navTitle.innerHTML = "<div class=\"cat-arrow cat-arrow__pers\"></div>hobbies";
+  }
+  catArrowPersonal = document.querySelector(".cat-arrow__pers");
 }
 
 
@@ -59,23 +132,27 @@ educationFolder.addEventListener('click', function() {
   document.querySelector(".nav-tree__education_school").classList.toggle("invisible");
   document.querySelector(".nav-tree__education_university").classList.toggle("invisible");
 });
+workFolder.addEventListener('click', function() {
+  workFolder.firstChild.classList.toggle("folder-arrow_opened");
+  document.querySelector(".nav-tree__work_catering").classList.toggle("invisible");
+});
 
 
 let contentText = document.querySelector(".content-window-text");
 let tabTitle = document.querySelector(".content-tab");
 let slider = document.querySelector(".content-window-slider");
 
-function li(string) {
-  return "<li class=\"content-window-text__stroke\">" + string + "</li>";
 
-}
 
-let childhood = li("childhood");
-let present = li("present");
-let code = li("code");
-let tech = li("tech");
-let school = li("school");
-let university = li("university");
+let childhood = toList("childhood");
+let present = toList("present");
+let code = toList("code");
+let tech = toList("tech");
+let school = toList("school");
+let university = toList("university");
+
+let catering = toList("catering");
+
 
 document.querySelector(".nav-tree-content").addEventListener("click", function(e) {
   
@@ -120,4 +197,41 @@ document.querySelector(".nav-tree-content").addEventListener("click", function(e
     contentText.innerHTML = university;
     openTab();
   }
+
+  else if (e.target.classList.contains("nav-tree__work_catering")) {
+    tabTitle.innerHTML = "catering" + "<button class=\"close\">x</button>";
+    contentText.innerHTML = catering;
+    openTab();
+  }
+
+let closeButton = document.querySelector(".close");
+
+closeButton.addEventListener('click', function() {
+  tabTitle.style.visibility = "hidden";
+  slider.style.visibility = "hidden";
+  contentText.style.visibility = "hidden";
 });
+});
+
+
+let catPersonal = document.querySelector(".nav-tree__title_pers");
+let catContacts = document.querySelector(".nav-tree__title_contacts");
+
+let catArrowPersonal = document.querySelector(".cat-arrow__pers");
+let catArrowContacts = document.querySelector(".cat-arrow__contacts");
+
+let navTreePersonal = document.querySelector(".nav-tree-content");
+let navTreeContactsEmail = document.querySelector(".nav-contacts__email");
+let navTreeContactsPhone = document.querySelector(".nav-contacts__phone");
+
+catPersonal.addEventListener('click', function() {
+  catArrowPersonal.classList.toggle("cat-arrow_opened");
+  navTreePersonal.classList.toggle("invisible");
+});
+
+catContacts.addEventListener('click', function() {
+  catArrowContacts.classList.toggle("cat-arrow_opened");
+  navTreeContactsEmail.classList.toggle("invisible");
+  navTreeContactsPhone.classList.toggle("invisible");
+});
+
---
+++ script/snake.js
@@ -0,0 +1,300 @@
+function getRandomCoord(max) {
+  // returns random coordinate from 0 to max
+  let num = Math.random() * max;
+  return Math.floor(num);
+}
+
+function startGame() {
+  run = setInterval(game, 1000/speed);
+  startButton.setAttribute("hidden", "")
+  startButton.removeEventListener('click', startGame);
+  startButton.addEventListener('click', function() {
+    clearInterval(run);
+    clearPosition();
+    startGame();
+  });
+  
+}
+
+function clearPosition() {
+  // reset snake position, apple position, score, game over overlay
+
+  applePosition = [getRandomCoord(width), getRandomCoord(height)];
+  score = 0;
+  resetScoreFields();
+  direction = 1;
+  snakeBody = []
+  // centering snake body
+  for (let i = 0; i < startLenght; i++) {
+    snakeBody.push([Math.floor(width/2), Math.floor(height/2)+1]);
+  }
+  gameOver.setAttribute("hidden", "");
+}
+
+function resetScoreFields() {
+  for (let food of foods) {
+    food.style.opacity = 1;
+  }
+}
+
+function waitScreen() {
+
+  // make everything shine
+  context.shadowColor=appleColor;
+  context.shadowBlur=10;
+
+  // clearing game field
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "rgba(1, 22, 39, 0.84)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+  
+  renderSnake();
+}
+
+function renderSnake() {
+  for (let i = 0; i < snakeBody.length; i++) {
+    // imitating fade-out of snake from head to tail
+    // // // // why is this four-block snake has only 3 blocks visible>? am i so bad in math?
+    currAlpha = 1 - i * (1 / (snakeBody.length));
+    context.fillStyle = "rgba(67, 217, 173, " + currAlpha + ")";
+
+    // making a round head depending on direction
+    if (i == 0) {
+      // only first block really needs in head rounding
+      context.beginPath();
+      if (direction == 1) {
+        context.arc(snakeBody[0][0]*10+5, snakeBody[0][1]*10, 5, 0, 2 * Math.PI, false);
+        context.fillRect(snakeBody[0][0] * blockSize, snakeBody[0][1] * blockSize, 10, 10);
+      }
+      else if (direction == 2) {
+        context.arc(snakeBody[0][0]*10+10, snakeBody[0][1]*10+5, 5, 0, 2 * Math.PI, false);
+        context.fillRect(snakeBody[0][0] * blockSize, snakeBody[0][1] * blockSize, 10, 10);
+      }
+      else if (direction == 3) {
+        context.arc(snakeBody[0][0]*10+5, snakeBody[0][1]*10+10, 5, 0, 2 * Math.PI, false);
+        context.fillRect(snakeBody[0][0] * blockSize, snakeBody[0][1] * blockSize, 10, 10);
+      }
+      else if (direction == 4) {
+        context.arc(snakeBody[0][0]*10, snakeBody[0][1]*10+5, 5, 0, 2 * Math.PI, false);
+        context.fillRect(snakeBody[0][0] * blockSize, snakeBody[0][1] * blockSize, 10, 10);
+      }
+      context.fill();
+    }
+    else {
+      // non-head blocks
+      context.fillRect(snakeBody[i][0] * blockSize, snakeBody[i][1] * blockSize, 10, 10);
+    }
+}
+}
+
+function drawStartGameText() {
+  startButton.innerHTML = "start-game";
+}
+
+function drawStartAgainButton() {
+  startButton.innerHTML = "start-again";
+  startButton.removeAttribute("hidden");
+  startButton.classList.add("start-again-button")
+}
+
+function drawPlayAgainButton() {
+  startButton.innerHTML = "play-again";
+  startButton.removeAttribute("hidden");
+  startButton.classList.add("start-again-button")
+}
+
+function drawGameOver() {
+  gameOver.innerHTML = "game over!";
+  gameOver.removeAttribute("hidden");
+}
+
+function drawWellDone() {
+  gameOver.innerHTML = "well done!";
+  gameOver.removeAttribute("hidden");
+  navBar.classList.remove("hidden");
+}
+// main game function
+game = function() {
+
+  // make everything shine
+  context.shadowColor=appleColor;
+  context.shadowBlur=10;
+
+  // clearing game field
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = "rgba(1, 22, 39, 0.84)";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  // snake behaviour
+  // direction
+  if (direction == 1) {
+    snakeBody.unshift([snakeBody[0][0], --snakeBody[0][1]]);
+  }  else if (direction == 2) {
+      snakeBody.unshift([++snakeBody[0][0], snakeBody[0][1]]);
+  } else if (direction == 3) {
+      snakeBody.unshift([snakeBody[0][0], ++snakeBody[0][1]]);
+  } else if (direction == 4) {
+      snakeBody.unshift([--snakeBody[0][0], snakeBody[0][1]]);
+  }
+  snakeBody.pop();
+
+  // rendering
+  renderSnake();
+  
+  // end game conditions
+  //score = 10
+  if (score == 10) {
+    drawWellDone();
+    drawPlayAgainButton();
+    clearInterval(run);
+  }
+
+  // snake get out of field
+  if (snakeBody[0][0] * 10 > canvas.width-1 || snakeBody[0][1] * 10 > canvas.height-1 || snakeBody[0][0] * 10 < 0 || snakeBody[0][1] * 10 < 0) {
+    drawStartAgainButton();  
+    drawGameOver();
+    clearInterval(run);
+      // alert("Score: " + score + '\nerr: out of field');
+  }
+  // snake touches itself
+  snakeBody.forEach(function(e, i) {
+    let lastBlock = snakeBody.length - 1;
+    if (e[0] == snakeBody[lastBlock][0] && e[1] == snakeBody[lastBlock][1] && i < lastBlock && score >= untouchableScore) {
+      drawStartAgainButton();
+      drawGameOver();
+      clearInterval(run);
+      // alert("Score: " + score + '\nerr: self-touch');
+    } 
+  })
+
+  // apple spawn
+  
+  // context.fillStyle = appleColor;
+  // context.fillRect(applePosition[0] * blockSize, applePosition[1] * blockSize, 10, 10);
+  context.beginPath();
+  context.arc(applePosition[0]*10+5, applePosition[1]*10+5, 5, 0, 2 * Math.PI, false);
+  context.fillStyle = appleColor;
+  context.fill();
+
+
+
+  // new apple if it was picked 
+  if (snakeBody[0][0] == applePosition[0] && snakeBody[0][1] == applePosition[1]) {
+    snakeBody.push(snakeBody[snakeBody.length - 1]);
+    applePosition[0] = getRandomCoord(width);
+    applePosition[1] = getRandomCoord(height);
+    score++;
+    foods[10-score].style.opacity = 0.4;
+  };
+}
+
+// arrow keys listener
+document.addEventListener("keydown", function(e) {
+  // if key is arrow right
+  if (e.keyCode == "39" && direction !== 4) {
+    e.preventDefault();
+      document.querySelector(".snake-game__info_arrows-arrowright").style.opacity = 0.6;
+      direction = 2; 
+  }
+  // if key is arrow down
+  if (e.keyCode == "40" && direction !== 1) {
+    e.preventDefault();
+    document.querySelector(".snake-game__info_arrows-arrowdown").style.opacity = 0.6;
+      direction = 3; 
+  }
+  // if key is arrow left
+  if (e.keyCode == "37" && direction !== 2) {
+    e.preventDefault();
+    document.querySelector(".snake-game__info_arrows-arrowleft").style.opacity = 0.6;
+
+      direction = 4; 
+  }
+  // if key is arrow up
+  if (e.keyCode == "38" && direction !== 3) {
+    document.querySelector(".snake-game__info_arrows-arrowup").style.opacity = 0.6;
+    e.preventDefault();
+      direction = 1; 
+  }
+})
+
+document.addEventListener("keyup", function(e) {
+  // if key is arrow right
+  if (e.keyCode == "39") {
+    e.preventDefault();
+    document.querySelector(".snake-game__info_arrows-arrowright").style.opacity = 1;
+  }
+  // if key is arrow down
+  if (e.keyCode == "40") {
+    e.preventDefault();
+    document.querySelector(".snake-game__info_arrows-arrowdown").style.opacity = 1;
+  }
+  // if key is arrow left
+  if (e.keyCode == "37") {
+    e.preventDefault();
+    document.querySelector(".snake-game__info_arrows-arrowleft").style.opacity = 1;
+  }
+  // if key is arrow up
+  if (e.keyCode == "38") {
+    document.querySelector(".snake-game__info_arrows-arrowup").style.opacity = 1;
+    e.preventDefault();
+  }
+})
+
+const speed = 4;  // moves per second
+const untouchableScore = 1; // minimum score to self-touch end game // 0 sets instant death(bug)
+const startLenght = 4; // initial snake body lenght
+
+const appleColor = "rgb(67, 217, 173)";
+
+
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+
+let score = 0;
+let blockSize = 10;
+//               🠙 1 (up) default
+//  (left) 4 🠘    🠚 2 (right)  
+//             🠛 3 (down)
+let direction = 1;
+
+let width = canvas.width/blockSize;
+let height = canvas.height/blockSize;
+
+// foods left counter
+let foods = document.getElementsByClassName('snake-game__info_foods-food');
+
+
+
+let startButton = document.getElementById('start-game__button');
+startButton.addEventListener('click', startGame);
+let skipButton = document.querySelector('.skip');
+
+
+let gameOver = document.getElementById("game-over");
+
+let navBar = document.querySelector(".header-nav");
+
+// initial clearing
+clearPosition();
+waitScreen();
+
+
+skipButton.addEventListener('click', function() {
+  navBar.classList.remove("hidden");
+  score = 10;
+  clearInterval(run);
+
+  drawWellDone();
+})  
+
+
+
+
+
+
+
+
+
+
+
+
